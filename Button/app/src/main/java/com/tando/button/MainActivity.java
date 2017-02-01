dc3fb7101e0ac7cccbf6f691262b22141b2dcc0b
Tan Do <tando@Tans-MacBook-Pro.local>
2017-01-31 18:09:25 -0800

    this is my first program for android, button has its function when click.

---
+++ Button/app/src/main/java/com/tando/button/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tando.button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void buttonFunc (View newView) {
+        Log.i("Info", "Button clicked");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
