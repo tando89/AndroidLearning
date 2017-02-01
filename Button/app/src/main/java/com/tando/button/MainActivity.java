a35d40debf8f4d3214460ffad5aed285617eb8f8
Tan Do <tando@Tans-MacBook-Pro.local>
2017-01-31 20:48:17 -0800

    add a function that when the user enter the data in the text field, then click the button, the data will display on screen

--- Button/app/src/main/java/com/tando/button/MainActivity.java
+++ Button/app/src/main/java/com/tando/button/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public void buttonFunc (View newView) {
-        Log.i("Info", "Button clicked");
+
+        EditText textField = (EditText) findViewById(R.id.textField);
+        Toast.makeText(MainActivity.this, "Hello" + " " + textField.getText().toString(), Toast.LENGTH_SHORT).show();
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
