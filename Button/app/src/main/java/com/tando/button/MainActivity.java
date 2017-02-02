2ab210eeb04837d42ef34510c0ad491f3281112f
Tan Do <tando@Tans-MacBook-Pro.local>
2017-02-02 10:24:00 -0800

    added uptate images

--- Button/app/src/main/java/com/tando/button/MainActivity.java
+++ Button/app/src/main/java/com/tando/button/MainActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public void buttonFunc (View newView) {
-
+        //R.id.textField R stands for resources. Meaning that it will search for id named textField
         EditText textField = (EditText) findViewById(R.id.textField);
         Toast.makeText(MainActivity.this, "Hello" + " " + textField.getText().toString(), Toast.LENGTH_SHORT).show();
 
+    }
+    public void buttonUpdate (View viewUpdate) {
+        ImageView cat = (ImageView) findViewById(R.id.imageCat01);
+        cat.setImageResource(R.drawable.img02);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
