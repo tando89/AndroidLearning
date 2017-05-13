94bcc1647c783318b9c4f336661fb4dab62e0ff4
tan <tantdo89@gmail.com>
2017-05-13 13:24:22 -0700

    parsing JSON objects into listview

---
+++ Events/app/src/main/java/com/example/tan/events/PassportValidation.java
@@ -0,0 +1,13 @@
+package com.example.tan.events;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PassportValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passport_validation);
+    }
+}
