b6ea8ac59a87e6ea057643dc7e08fc113a9a374d
tan <tantdo89@gmail.com>
2017-05-20 17:35:26 -0700

    Map Advanced

---
+++ MapAdv/app/src/main/java/com/example/tan/mapadv/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.tan.mapadv;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.lstView);
+
+        final ArrayList<String> places = new ArrayList<>();
+
+        places.add("Add a new place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> AdapterView, View view, int i, long l) {
+
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("PlaceNumber", i);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
