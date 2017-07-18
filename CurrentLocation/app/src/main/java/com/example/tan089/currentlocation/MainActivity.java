8761c22ce194b1f1cb21340488ce633c6e7b442a
tando89 <tantdo89@gmail.com>
2017-07-17 20:13:37 -0700

    Added report to server function but the app crashed

--- CurrentLocation/app/src/main/java/com/example/tan089/currentlocation/MainActivity.java
+++ CurrentLocation/app/src/main/java/com/example/tan089/currentlocation/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tan089.currentlocation;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -12,20 +13,38 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 //Must give permission ACCESS_FINE_LOCATION in the manifest
 public class MainActivity extends AppCompatActivity {
     //Declare LocationManager and LocationListener
     LocationManager locationManager;
     LocationListener locationListener;
+    String address = "Unable to get the address!";
+    EditText Message;
+    AlertDialog.Builder builder;
+    String server_url ="https://schoolserver-tand089.c9users.io/Report.php";
     //Process when users give the permission
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -60,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         //Using try/catch to prevent the app from crashing when failing to get Addresses
         try {
             //Declare the error string
-            String address = "Unable to get the address!";
+            //String address = "Unable to get the address!";
             List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
             //Check if the address is valid
             if (listAddresses != null && listAddresses.size() > 0) {
@@ -90,17 +109,20 @@ public class MainActivity extends AppCompatActivity {
                 TextView addressTextView = (TextView)  findViewById(R.id.txtAddress);
                 //set the address into the text View
                 addressTextView.setText(address);
-                Log.i("Address", address.toString());
+                //Log.i("Address", address.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //set up locationManager and locationListener above and cast them into their type
         //Using built-in location service
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -110,9 +132,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationChanged(Location location) {
                 //call the updated location above
-                updatedLocationInfo(location);
-            }
 
+                        updatedLocationInfo(location);
+            }
+//
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
 
@@ -149,5 +172,66 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
     }
+
+   /*
+   public void bntTap (View bnt) {
+        Message = (EditText) findViewById(R.id.message);
+        //Hide virtual keyboard after click the button
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        final String message;
+
+        message = Message.getText().toString();
+        if (message.equals("")) {
+            builder.setTitle("Unable to report");
+            //Creating a AlertDialog to display errors
+            AlertDialog alertDialog = builder.create();
+            alertDialog.setMessage("Please Enter All Required Fields*");
+            alertDialog.show();
+        } else {
+            //Post request
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            //get response form server to check if it is successfully submitted
+                            builder.setTitle("Server Response");
+                            builder.setMessage("Congratulation" + " " + response);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Message.setText("");
+                                }
+                            });
+                            AlertDialog alertDialog = builder.create();
+                            alertDialog.show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(MainActivity.this, "Error!!!", Toast.LENGTH_SHORT).show();
+                            error.printStackTrace();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    //send data to mySQL server
+                    //the keys must be same as field names in mySQL server
+                    params.put("LOCATION", address);
+                    params.put("MESSAGES", message);
+                    return params;
+                }
+            };
+            MySingleton.getInstance(MainActivity.this).addTorequestqueue(stringRequest);
+            //Log.i("Address", address.toString());
+        }
+    } */
 }
