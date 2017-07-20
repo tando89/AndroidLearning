befe984577e26c206fa544897d6ef119c21559c3
tando89 <tantdo89@gmail.com>
2017-07-20 15:58:25 -0700

    Using firebase to send push notitification

---
+++ PushNotification/app/src/main/java/com/example/tan089/pushnotification/MyFirebaseIDService.java
@@ -0,0 +1,32 @@
+package com.example.tan089.pushnotification;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+
+
+/**
+ * Created by tan089 on 7/20/2017.
+ */
+
+public class MyFirebaseIDService extends FirebaseInstanceIdService{
+
+    private static String TAG = "InstanceID";
+
+    @Override
+    public void onTokenRefresh() {
+        //Get updated InstanceID token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed Token:" + refreshedToken);
+
+        // TODO: Send any registration to the app servers
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
