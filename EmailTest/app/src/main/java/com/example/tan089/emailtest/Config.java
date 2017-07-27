ff0c745637bd62fae17a21faa41642ee464d03a8
tando89 <tantdo89@gmail.com>
2017-07-27 16:46:19 -0700

    Send email with gmail api

---
+++ EmailTest/app/src/main/java/com/example/tan089/emailtest/Config.java
@@ -0,0 +1,10 @@
+package com.example.tan089.emailtest;
+
+/**
+ * Created by tan089 on 7/27/2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="robottestSOS@gmail.com";
+    public static final String PASSWORD ="Testing123";
+}
