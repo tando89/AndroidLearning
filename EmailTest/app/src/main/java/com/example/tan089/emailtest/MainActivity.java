d614e06f0e5115573eac4790001eb7f71d940f6a
tando89 <tantdo89@gmail.com>
2017-07-27 17:34:04 -0700

    Finished

--- EmailTest/app/src/main/java/com/example/tan089/emailtest/MainActivity.java
+++ EmailTest/app/src/main/java/com/example/tan089/emailtest/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.tan089.emailtest;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.Properties;
 
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
     //Declaring EditText
-    private EditText editTextEmail;
     private EditText editTextSubject;
     private EditText editTextMessage;
 
@@ -25,18 +26,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Initializing the views
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextSubject = (EditText) findViewById(R.id.editTextSubject);
         editTextMessage = (EditText) findViewById(R.id.editTextMessage);
 
         buttonSend = (Button) findViewById(R.id.buttonSend);
 
+        //hide keyboard
+        editTextSubject.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKyeboard(v);
+                }
+            }
+        });
+        editTextMessage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKyeboard(v);
+                }
+            }
+        });
+
         //Adding click listener
-        buttonSend.setOnClickListener(this);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editTextSubject.length() <= 0 || editTextMessage.length() <= 0){
+                    Toast.makeText(getApplicationContext(), "Please fill out all the required fields*", Toast.LENGTH_LONG).show();
+                }
+                else {
+
+                    AlertDialog.Builder user_alert_tosend = new AlertDialog.Builder(MainActivity.this);
+                    user_alert_tosend.setMessage("Once you click continue, you can't cancel it. Tap \"Continue\" to send")
+                            .setPositiveButton("Continue", alertdialog_oncontinue)
+                            .setNegativeButton("Stop", null)
+                            .setCancelable(false).show();
+
+
+                }
+
+            }
+        });
     }
+    protected DialogInterface.OnClickListener alertdialog_oncontinue = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            sendEmail();
+            editTextMessage.setText("");
+            editTextSubject.setText("");
+            //send to mainpage when done
+            Intent page = new Intent(MainActivity.this, Page2.class);
+            startActivity(page);
+        }
+    };
     private void sendEmail() {
         //Getting content for email
-        String email = editTextEmail.getText().toString().trim();
+        String email = "tantdo89@gmail.com";
         String subject = editTextSubject.getText().toString().trim();
         String message = editTextMessage.getText().toString().trim();
 
@@ -47,8 +94,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sm.execute();
     }
 
-    @Override
-    public void onClick(View v) {
-        sendEmail();
+    public void hideKyeboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
