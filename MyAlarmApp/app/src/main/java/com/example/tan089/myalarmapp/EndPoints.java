8323ce4db57f676e542063cd2658da9530631afc
tando89 <tantdo89@gmail.com>
2017-09-20 19:41:14 -0700

    fcm notification php and firebase

---
+++ MyAlarmApp/app/src/main/java/com/example/tan089/myalarmapp/EndPoints.java
@@ -0,0 +1,12 @@
+package com.example.tan089.myalarmapp;
+
+/**
+ * Created by tan089 on 9/20/2017.
+ */
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "https://schoolserver-tand089.c9users.io/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "https://schoolserver-tand089.c9users.io/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "https://schoolserver-tand089.c9users.io/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = " https://schoolserver-tand089.c9users.io/GetRegisteredDevices.php";
+}
---
+++ MyAlarmApp/app/src/main/java/com/example/tan089/myalarmapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,30 @@
+package com.example.tan089.myalarmapp;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by tan089 on 9/20/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        storeToken(refreshedToken);
+    }
+    private void storeToken(String token) {
+        //we will save the token in sharedpreferences later
+        //saving the token on shared preferences
+        SharedPrefManager.getInstance(getApplicationContext()).saveDeviceToken(token);
+    }
+}
