fe615614af865e784d9e500598d176ff62513ebc
tando89 <tantdo89@gmail.com>
2017-10-18 17:20:24 -0700

    download file using download manager

---
+++ downloadFile/app/src/main/java/com/example/tan089/downloadfile/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tan089.downloadfile;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bnt;
+    DownloadManager downloadManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bnt = (Button) findViewById(R.id.bnt01);
+        bnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse("https://alarmserver-tand089.c9users.io/ga.xls");
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                Long reference = downloadManager.enqueue(request);
+            }
+        });
+    }
+}
